ea0018fe227aec4ff47917f2d1543e7aa1d2d4e8
Jake Abed <jakeabed@proton.me>
2025-02-05 09:53:28 -0800

    empty out for redo

--- routes/index.tsx
+++ routes/index.tsx
@@ -19,41 +19,9 @@ export default function Home() {
         >
         </meta>
       </Head>
-      <body className="w-screen min-h-screen text-white flex font-fira flex-col bg-slate-900 overflow-x-hidden">
-        <Header />
-        <Intro></Intro>
-        <h2 className="text-center pt-8 text-3xl leading-[1.6rem] font-extrabold">
-          I Made These
-        </h2>
-        <div
-          id="projects"
-          className="flex justify-center py-12 lg:py-12 pb-8 gap-6 flex-wrap"
-        >
-          <div className="lg:w-8/12 flex justify-center gap-6 flex-wrap">
-            {projects.map((project) => {
-              return (
-                <Project
-                  className="p-4 flex flex-col items-center bg-deep-black flex-wrap"
-                  {...project}
-                >
-                </Project>
-              );
-            })}
-          </div>
-        </div>
-        <Technologies technologies={technologies} />
-        <Contact></Contact>
-        <AboutMe />
-        <div className="static bottom-0 w-full p-4 mt-auto bg-black flex justify-center">
-          <a href="https://fresh.deno.dev">
-            <img
-              width="197"
-              height="37"
-              src="https://fresh.deno.dev/fresh-badge.svg"
-              alt="Made with Fresh"
-            />
-          </a>
-        </div>
+      <body className="w-screen min-h-screen text-white gap-8 flex justify-center font-fira flex-col bg-slate-900 overflow-x-hidden">
+          <h1 className="text-center">jake abed's 'old' portfolio</h1>
+          <p className="text-center">new portfolio on the way shortly</p>
       </body>
     </>
   );
